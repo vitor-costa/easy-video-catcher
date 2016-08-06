a1398759c8a522a15e4aef86e5c9ce085e05f80f
Vitor Costa <vitor.costa@corp.globo.com>
2016-08-06 15:45:27 -0300

    Increase delays

--- video_catcher.py
+++ video_catcher.py
@@ -1,3 +1,5 @@
+# !/usr/bin/env python3
+
 from selenium import webdriver
 import sys
 import time
@@ -10,6 +12,7 @@ driver.get('https://www.youtube.com/?gl=BR')
 search_box = driver.find_element_by_id('masthead-search-term')
 search_box.send_keys(video_name)
 search_box.submit()
+time.sleep(1)
 video = driver.find_elements_by_class_name('yt-uix-tile')[0]
 video.click()
 video_url = driver.current_url
@@ -26,7 +29,7 @@ converter_box.send_keys(video_url)
 converter_button = driver.find_element_by_id('grab1')
 converter_button.click()
 # wait video processing...
-time.sleep(5)
+time.sleep(20)
 convert_button = driver.find_element_by_id('convertmp3')
 convert_button.click()
 time.sleep(5)
@@ -35,4 +38,3 @@ download_button.click()
 # wait video download...
 time.sleep(60)
 driver.close()
-
